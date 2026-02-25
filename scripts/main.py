84597a675e3da21a89c2e3edc35596040867e6cb
JB5005 <joaobentesacord@gmail.com>
2026-02-25 12:43:48 +0000

    Initial commit: Add main script

---
+++ scripts/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Scalable Bioinformatics Pipeline Orchestrator - Main Script"""
+
+def main():
+    print("Hello, Bioinformatics Pipeline!")
+
+if __name__ == "__main__":
+    main()
